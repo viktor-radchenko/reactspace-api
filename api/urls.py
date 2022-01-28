93cd0e3c64d932bf1e93c9e3d1d31590fae37857
Viktor Radchenko <viktor.radchenko@playtech.com>
2022-01-28 16:06:39 +0400

    updated static files and routes

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path('get_all_products', views.get_all_products, name="get_all_products"),
+    path('get_product/<str:pk>', views.get_product, name="get_product"),
 ]
